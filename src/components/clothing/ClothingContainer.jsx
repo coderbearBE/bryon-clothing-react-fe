9af973f65c4885c42d0fa5762d29ea87ff060ed8
Kevin Bervoets <coderbear@proton.me>
2024-02-14 22:25:09 +0100

    New UI for cleaner look and feel

--- src/components/clothing/ClothingContainer.jsx
+++
@@ -1,9 +0,0 @@
-import { Flex } from "@chakra-ui/react";
-
-export const ClothingContainer = ({ children }) => {
-  return (
-    <Flex direction="column" aligng="stretch" wrap="wrap" bg="lightGray" p={8}>
-      {children}
-    </Flex>
-  );
-};
