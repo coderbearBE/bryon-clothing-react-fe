c13aff85b7bcf6cd65c9c5831e1cf9b1b48f6621
coderbearBE <111735148+coderbearBE@users.noreply.github.com>
2024-05-01 22:40:37 +0200

    Improvement/clothing items (#9)
    * improvement(nav): changed default exports to named exports
    
    * New UI for cleaner look and feel
    
    * improvement(Login): Change logo background to match new UI style
    
    * improvement(Login): match new UI style
    
    * fix(login): correct error handling when login fail
    
    * fix(routing): added authorization route guard to routing architecture
    
    * fix(routing): correct import dependencies order
    
    * improvement(packages): node upgrade
    
    * improvement(UserContext): Error handling when connection lost
    
    * feature(profile): add padding on main view
    
    * feature(profile): Added profile header to FE view
    
    * feature(clothingItems): Show available products based on gender
    
    * feature(clothingItems): Show correct sizes for clothingType
    
    * feature(slideIn): created an overview when selected clothingItems and confirmed
    
    * feature(slideIn): improved design order overview
    
    * feature(slideIn): remove console.log
    
    * feature(Clothing): able to create an order with validation and user feedback

--- src/components/clothing/ClothingList.jsx
+++ src/components/clothing/ClothingList.jsx
@@ -1,117 +1,274 @@
 import {
+  Box,
   Button,
   HStack,
   Heading,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
+  Input,
   Select,
   Stack,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import * as R from "ramda";
+import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import useSWR from "swr";
 
 import { useAxios } from "../../hooks";
 import { SlideIn } from "../../shared/components";
-import { sizes } from "../../shared/constants/enums";
+import { UserContext } from "../../shared/context/UserContext";
+import {
+  accessorySizes,
+  shoeSizes,
+  sizes,
+  underwearSizes,
+} from "../../shared/constants";
 
 export const ClothingList = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [currentOrder, setCurrentOrder] = useState({});
-  
-  const { get } = useAxios();
-  const {data: clothingItems} = useSWR(
-    '/products',
+  const [filteredClothingItems, setFilteredClothingItems] = useState([]);
+  const [currentOrderItems, setCurrentOrderItems] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [restAmount, setRestAmount] = useState(0);
+
+  const { get, post } = useAxios();
+  const { user } = useContext(UserContext);
+  const { data: clothingItems } = useSWR(
+    "/products",
     async (path) => await get(path),
-    { suspense: true, }
+    { suspense: true }
   );
-  const { handleSubmit, register } = useForm();
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    reset,
+  } = useForm();
+
+  const toast = useToast();
+
+  useEffect(() => {
+    if (clothingItems.length !== 0) {
+      setFilteredClothingItems(
+        clothingItems.filter(
+          (item) => R.isNil(item.retailFor) || item.retailFor === user.gender
+        )
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [clothingItems]);
+
+  useEffect(() => {
+    if (currentOrderItems.length !== 0) {
+      let amount = 0;
+      for (const item of currentOrderItems) {
+        amount = amount + item.quantity * item.price;
+      }
+
+      setTotalAmount(amount);
+
+      const rest = user.budget - amount;
+      setRestAmount(rest);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentOrderItems]);
 
   const toggleSlideIn = () => setIsOpen(!isOpen);
 
-  const handleOrderSubmit = (orderData) => {
-    console.log("Order preview (keys)", Object.keys(orderData));
-    console.log("Order preview (values)", Object.values(orderData));
-    console.log("Order preview (entries)", Object.entries(orderData));
-    const filtered = Object.entries(orderData).filter(
-      (entry) => entry[1] !== "0"
+  const renderSelectOptions = (item) => {
+    switch (item.clothingType) {
+      case "ACCESSORY":
+        return (
+          <>
+            {accessorySizes.map((size) => (
+              <option key={size} value={size}>
+                {size}
+              </option>
+            ))}
+          </>
+        );
+      case "FOOTWEAR":
+        return (
+          <>
+            {shoeSizes.map((size) => (
+              <option key={size} value={size}>
+                {size}
+              </option>
+            ))}
+          </>
+        );
+      case "UNDERWEAR":
+        return (
+          <>
+            {underwearSizes.map((size) => (
+              <option key={size} value={size}>
+                {size}
+              </option>
+            ))}
+          </>
+        );
+      default:
+        return (
+          <>
+            {sizes.map((size) => (
+              <option key={size} value={size}>
+                {size}
+              </option>
+            ))}
+          </>
+        );
+    }
+  };
+
+  const transformFormData = (formData) => {
+    let order = [];
+    for (const item of formData) {
+      const orderedItem = clothingItems.find(
+        (clothingItem) => clothingItem.productCode === item[0]
+      );
+      order = [...order, { ...orderedItem, ...item[1] }];
+    }
+
+    return order;
+  };
+
+  const handleOrderOverview = (formData) => {
+    const formDataWithSizeAndQuantity = Object.entries(formData).filter(
+      (entry) => !R.isEmpty(entry[1].size) && entry[1].quantity !== 0
     );
-    console.log(filtered);
-    console.log("clothingItems", clothingItems);
-    // TODO setCurrentOrder and pass it to SlideIn component
+
+    const orderList = transformFormData(formDataWithSizeAndQuantity);
+    setCurrentOrderItems(orderList);
+
     toggleSlideIn();
   };
-  
+
+  const handleOrderSubmit = async () => {
+    const owner = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      budget: user.budget,
+    };
+
+    const order = {
+      owner,
+      year: new Date().getFullYear(),
+      products: currentOrderItems,
+      totalAmount,
+      restAmount,
+    };
+
+    try {
+      await post("/orders", order);
+
+      toast({
+        title: "Bedankt voor je bestelling!",
+        status: "success",
+        duration: 5000,
+        position: "bottom-left",
+        variant: "left-accent",
+      });
+    } catch (error) {
+      toast({
+        title: "Je hebt al een bestelling gemaakt voor dit jaar.",
+        status: "error",
+        duration: 5000,
+        position: "bottom-left",
+        variant: "left-accent",
+      });
+    }
+
+    reset();
+    toggleSlideIn();
+  };
+
   return (
     <>
-      <form onSubmit={handleSubmit(handleOrderSubmit)}>
+      <form onSubmit={handleSubmit(handleOrderOverview)}>
         <VStack spacing={4} w="full" h="full" align="stretch">
-          <Heading>ClothingList</Heading>
+          <Heading mb={4}>Kledij Bryon (Doltcini)</Heading>
 
-          {clothingItems.map((item) => (
+          {filteredClothingItems.map((item) => (
             <div key={item.id}>
               <Stack
                 direction={["column", "column", "row", "row"]}
                 justifyContent="space-between"
               >
-                <Text>
-                  {item.description_bryon} {item.additional_info}
-                </Text>
+                <Text>{item.descriptionBryon}</Text>
                 <HStack justifyContent="space-between">
                   <Text w="120px" fontWeight="bold" mr={8}>
-                    {item.price} EUR
+                    {item.price.toFixed(2)} EUR
                   </Text>
                   <Select
                     w="120px"
                     placeholder="Maat.."
-                    {...register(`${item.id}.size`)}
+                    {...register(`${item.productCode}.size`)}
                   >
-                    {sizes.map((size) => (
-                      <option key={size} value={size}>
-                        {size}
-                      </option>
-                    ))}
+                    {renderSelectOptions(item)}
                   </Select>
-
-                  <NumberInput
-                    defaultValue={0}
-                    min={0}
-                    max={15}
-                    precision={0}
-                    size="sm"
-                    maxW={16}
-                    {...register(`${item.id}.quantity`)}
-                  >
-                    <NumberInputField />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper>+</NumberIncrementStepper>
-                      <NumberDecrementStepper>-</NumberDecrementStepper>
-                    </NumberInputStepper>
-                  </NumberInput>
+                  <Input
+                    type="number"
+                    w="80px"
+                    focusBorderColor="blackAlpha.300"
+                    placeholder="0 - 15"
+                    _placeholder={{ color: "blackAlpha.900" }}
+                    {...register(`${item.productCode}.quantity`, {
+                      min: 0,
+                      max: 15,
+                      value: 0,
+                      valueAsNumber: true,
+                    })}
+                  />
                 </HStack>
               </Stack>
             </div>
           ))}
-          <Button
-            type="submit"
-            bg="fluoPink"
-            color="white"
-            size="md"
-            w={["full", "150px", "150px", "150px"]}
-            borderRadius="none"
-            _hover={{ opacity: 0.75 }}
-          >
-            Bevestigen
-          </Button>
+
+          {!R.isEmpty(errors) && (
+            <Box
+              as="p"
+              px={4}
+              py={2}
+              bg="red.100"
+              color="red.500"
+              border="2px"
+              borderColor="red.500"
+              fontSize="large"
+              fontStyle="italic"
+              fontWeight="bold"
+            >
+              Vul een getal tussen 0 en 15 in
+            </Box>
+          )}
+
+          <Box>
+            <Button
+              type="submit"
+              bg="fluoPink"
+              color="white"
+              size="md"
+              w={["full", "150px", "150px", "150px"]}
+              mt={6}
+              borderRadius="none"
+              _hover={{ opacity: 0.75 }}
+            >
+              Bevestigen
+            </Button>
+          </Box>
         </VStack>
       </form>
 
-      <SlideIn isOpen={isOpen} toggle={toggleSlideIn} />
+      <SlideIn
+        isOpen={isOpen}
+        toggle={toggleSlideIn}
+        orderedItems={currentOrderItems}
+        submitOrder={handleOrderSubmit}
+        totalAmount={totalAmount}
+        restAmount={restAmount}
+        userBudget={user.budget}
+      />
     </>
   );
-}
+};
